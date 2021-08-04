835d3219982f8846674a8b82e9e88099b5d327eb
Øystein Haug Olsen <oystein.olsen@cognite.com>
2021-08-04 10:40:53 +0200

    Power save

--- app.py
+++ app.py
@@ -1,54 +1,53 @@
-# https://github.com/SergeyPiskunov/micropython-hx711
-
 import sys
-from utime import sleep
-from machine import reset
+import machine
+import utime
 
 import board
-import config
+import mqtt
 import wifi
 
 from scales import scales
-from mqtt import client, topic
 
-# power save:
+
+# Power save:
+# * boot grace time - could depend on digital IO state - on pin with pull up/down
 # * LED blink
 # * prints
 # * sleeps
+# * power weight bridge - drive directly with pin? 3.3V over 2k is 1.5mA, should be ok. Test noise.
+# * ADC power
+
 
 def main():
     try:
         print("Running app.py")
-        board.led.value(1)
+        # board.led.value(1)
         wifi.connect()
-        print("Connecting to mqtt...")
-        client.connect()
-        sleep(1)
-        # TODO: should also power off the bridge - drive directly with pin? 5V over 2k is 3mA, should be ok. Test noise.
-        # Could use FET - if can power down the ESP...
+        # print("Connecting to mqtt...")
+        mqtt.client.connect()
         # scales.power_on()
         # sleep to stabilize...
         weight = scales.stable_value()
         scales.power_off()
         print("weight", weight)
-        client.publish(topic("weight"), bytes(str(weight), 'utf-8'))
+        mqtt.client.publish(mqtt.topic("weight"), bytes(str(weight), 'utf-8'))
     except Exception as e:
         sys.print_exception(e)
     finally:
         try:
-            client.disconnect()
+            mqtt.client.disconnect()
         except Exception:
             pass
-    board.led.value(0)
+    # board.led.value(0)
     if 1:
-        print("Deep sleep for 20 seconds in 5 seconds...")
-        sleep(5)
+        # print("Deep sleep for 20 seconds...")
+        # utime.sleep(5)
         board.deep_sleep(20)
     else:
         # debug
         print("Reset in 5 seconds...")
-        sleep(5)
-        reset()
+        utime.sleep(5)
+        machine.reset()
 
 
 if __name__ == "__main__":
